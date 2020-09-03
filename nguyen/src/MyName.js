31fd6fb45f5e1400c72d14796d382072077ce74c
manhtoan1501 <nguyenmanhtoan1501@gmail.com>
2020-09-03 14:49:54 +0700

    Nguyen commit code moi nhat

---
+++ nguyen/src/MyName.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function FunctionEntry () {
+  return (
+    <div>
+      <h1>My name: Nguyen</h1>
+    </div>
+  )
+}
+
+export default FunctionEntry;
